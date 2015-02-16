4a3f5116d76d302a3fef7c58cae9e27ccb06a4f6
Andrew Abogado <me@andrewabogado.com>
2015-02-17 00:59:43 +0800

    Try mimicing the partials

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 window.addEventListener('hashchange', function() {
 
-  console.log('Hash changed!: ' + window.location.hash);
+  if (window.location.hash === '#/bookmark/1') {
+    console.log('Page 1 is cool.');
+  }
+
+  if (window.location.hash === '#/bookmark/2') {
+    console.log('Going to page 2');
+  }
+
+  if (window.loation.hash === '#/bookmark/3') {
+    console.log('Lets start with 3');
+  }
 
 });
